011a2bd74f60a1671c6073853d45e5bb1cad6990
YAHIA <tm@gamil.com>
2026-08-11 15:20:20 +0300

    feat(menu): add help entry

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,12 @@
 const utils = require('./utils');
 
 const menu = {
-    title: 'Main Menu',
-    items: [
-        { id: 'home', label: 'Home' },
-        { id: 'about', label: 'About' }
-    ]
+  title: 'Main Menu',
+  items: [
+    { id: 'help', label: 'Help' },
+    { id: 'home', label: 'Home' },
+    { id: 'about', label: 'About' }
+  ]
 };
 
 module.exports = menu;
