bfec53b2e829718e43120295cd8da3d29058ce48
ahmed <am58008576@gmail.com>
2026-08-11 15:17:32 +0300

    src/main.js

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,1 @@
-const utils = require('./utils');
-
-const menu = {
-    title: 'Main Menu',
-    items: [
-        { id: 'home', label: 'Home' },
-        { id: 'about', label: 'About' }
-    ]
-};
-
-module.exports = menu;
+const items = ["profil","home"]
